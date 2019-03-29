75099c12f8a7f8937825b4d82eeb3a0a1a9943db
Magnus Hansen <Magnushansen12@hotmail.com>
2019-03-29 16:58:21 +0100

    Improve subscription functionality
    Adds commands for adding, removing and listing subscriptions.
    Adds a command for listing upcoming episodes of a series.

--- README.md
+++ README.md
@@ -1,8 +1,29 @@
 # drdl3
 DR Downloader 3
 
+Installation:
 ```
-usage: drdl3.py [-h] [-o OUTPUTDIR] [-t] [-s] [-c] [-j JOBID] url
+sudo apt install ffmpeg
+cd drdl3
+pip install -r requirements.txt
+```
+
+Commands:
+```
+usage: drdl3.py [-h] {dl,lsubs,add,rsubs,upcoming} ...
+
+positional arguments:
+  {dl,lsubs,add,rsubs,upcoming}
+                        Sub-command help
+    dl                  Download episodes
+    lsubs               List subscriptions
+    add                 Add new subscription
+    rsubs               Remove subscriptions
+    upcoming            List upcoming episodes
+```
+Downloading:
+```
+usage: drdl3.py dl [-h] [-o OUTPUTDIR] [-t] [-s] [--subscribe] url
 
 positional arguments:
   url                   Url to video from dr.dk/tv
@@ -13,8 +34,32 @@ optional arguments:
                         Specify output directory
   -t, --tvseries        Download all available in tv series
   -s, --season          Download all available in the season
-  -c, --crontab         Setup a crontab for downloading the next episode when
-                        it releases
-  -j JOBID, --jobid JOBID
-                        Cronjob id. Used for deleting old cronjobs
+  --subscribe           Subscribe to the episode, season or series.
 ```
+Subscribe:
+```
+usage: drdl3.py add [-h] [-o OUTPUTDIR] [-t] [-s] [-r [RATE]] url
+
+positional arguments:
+  url                   Url to video from dr.dk/tv
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -o OUTPUTDIR, --outputdir OUTPUTDIR
+                        Specify output directory
+  -t, --tvseries        Subscribe to the tv series
+  -s, --season          Subscribe to the season
+  -r [RATE], --rate [RATE]
+                        Set update rate to every x hours
+```
+Remove subscription:
+```
+usage: drdl3.py rsubs [-h] int [int ...]
+
+positional arguments:
+  int         Index of subscription
+
+optional arguments:
+  -h, --help  show this help message and exit
+```
+
--- drdl3.py
+++ drdl3.py
@@ -2,7 +2,7 @@
 
 import argparse
 import os
-import uuid
+import re
 from datetime import datetime
 
 import requests
@@ -14,12 +14,10 @@ PROGRAM_CARD_BASE_URL = 'https://www.dr.dk/mu-online/api/1.4/programcard/'
 BUNDLE_BASE_URL = 'https://www.dr.dk/mu/bundle/'
 
 
-def main(args):
+def download(args):
     # Get program card
     url = args.url
-    program_card_url = PROGRAM_CARD_BASE_URL + url[url.rfind('/') + 1:]
-    request = requests.get(program_card_url)
-    program_card = request.json()
+    program_card = program_card_from_url(url)
     dl_url_list = []
     if program_card.get('PresentationUri'):
         dl_url_list.append(program_card.get('PresentationUri'))
@@ -36,44 +34,9 @@ def main(args):
                 for episode in season.get('Episodes').get('Items'):
                     dl_url_list.append(episode.get('PresentationUri'))
 
-    # upcoming
-    if args.crontab:
-        bundle_url = BUNDLE_BASE_URL + program_card.get('SeriesUrn')
-        request = requests.get(bundle_url)
-        bundle_data = request.json()
-        bundle_data = bundle_data.get('Data')[0]
-        members = bundle_data.get('Relations')
-        members = [mem['Slug'] for mem in members if mem['Kind'] == 'Member' and 'BundleType' not in mem]
-
-        upcoming_release_dates = []
-        for member in members:
-            request = requests.get(PROGRAM_CARD_BASE_URL + member)
-            data = request.json()
-            date = parse(data.get('SortDateTime')).replace(tzinfo=None)
-            if data.get('PresentationUri') is None and date > datetime.now():
-                upcoming_release_dates.append(parse(data.get('SortDateTime')))
-
-        cron = CronTab(user=True)
-
-        if args.jobid:
-            cron.remove_all(comment=f'drdl3: {args.jobid}')
-        # If there are upcoming episodes create a new crontab
-        if min(upcoming_release_dates).replace(tzinfo=None) > datetime.now():
-            cron_id = str(uuid.uuid4())
-            job_args = args.url
-            job_args += f' -o {args.outputdir}' if args.outputdir else ''
-            job_args += f' -t' if args.tvseries else ''
-            job_args += f' -s' if args.season else ''
-            job_args += ' -c'
-            job_args += f' -j {cron_id}'
-            job = cron.new(command=' '.join([os.path.abspath(__file__), job_args]), comment=f'drdl3: {cron_id}')
-            job.setall(min(upcoming_release_dates))
-            cron.write()
-
     ydl_opts = {
         'write_all_thumbnails': True,
-        'writesubtitles': True,
-        'download_archive': 'youtube-dl-archive.txt'
+        'writesubtitles': True
     }
     if args.outputdir:
         os.chdir(args.outputdir)
@@ -82,15 +45,115 @@ def main(args):
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download(dl_url_list)
 
+    if args.subscribe:
+        add_subscription(args)
+
+
+def upcoming(args):
+    program_card = program_card_from_url(args.url)
+    bundle_url = BUNDLE_BASE_URL + program_card.get('SeriesUrn')
+    request = requests.get(bundle_url)
+    bundle_data = request.json()
+    bundle_data = bundle_data.get('Data')[0]
+    members = bundle_data.get('Relations')
+    members = [mem['Slug'] for mem in members if mem['Kind'] == 'Member' and 'BundleType' not in mem]
+
+    for member in members:
+        request = requests.get(PROGRAM_CARD_BASE_URL + member)
+        data = request.json()
+        date = parse(data.get('SortDateTime')).replace(tzinfo=None)
+        if data.get('PresentationUri') is None and date > datetime.now():
+            print('[drdl3]', data.get('SortDateTime'), data.get('Title'))
+
+
+def list_subscriptions(args):
+    cron = CronTab(user=True)
+    subscriptions = cron.find_command('drdl3.py')
+    for index, sub in enumerate(subscriptions):
+        m = re.search(r'[^.\s]*https?://(?:www\.)?dr\.dk/(?:tv/se|nyheder|radio/ondemand)/(?:[^/\s]+/?)*', sub.command)
+        url = sub.command[m.start():m.end()]
+        cmd_args = sub.command.split()
+        outdir = '-o' in cmd_args
+        tvseries = '-t' in cmd_args
+        season = '-s' in cmd_args
+        program_card = program_card_from_url(url)
+        result = f'[drdl3] {index} '
+
+        if tvseries:
+            result += f'TV Series: {program_card["SeriesTitle"]}. '
+        elif season:
+            result += f'Season {program_card["SeasonNumber"]} of {program_card["SeriesTitle"]}. '
+        else:
+            result += f'{program_card["Title"]} '
+        result += 'Output directory: ' + cmd_args[cmd_args.index('-o') + 1] if outdir else 'default'
+        print(result)
+
+
+def add_subscription(args):
+    cron = CronTab(user=True)
+    job_args = 'dl ' + args.url
+    job_args += f' -o {args.outputdir}' if args.outputdir else ''
+    job_args += f' -t' if args.tvseries else ''
+    job_args += f' -s' if args.season else ''
+    job_args += f' >> {os.path.dirname(os.path.abspath(__file__)) + os.sep}log.txt'
+    job = cron.new(command=' '.join([os.path.abspath(__file__), job_args]))
+    job.setall(f'0 */{args.rate} * * *')
+    cron.write()
+
+
+def remove_subscription(args):
+    cron = CronTab(user=True)
+    jobs = cron.find_command('drdl3.py')
+    jobs = [job for index, job in enumerate(jobs) if index in args.subscriptions]
+    for job in jobs:
+        cron.remove(job)
+    cron.write()
+
+
+def program_card_from_url(url):
+    program_card_url = PROGRAM_CARD_BASE_URL + url[url.rfind('/') + 1:]
+    request = requests.get(program_card_url)
+    return request.json()
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('url', help='Url to video from dr.dk/tv')
-    parser.add_argument('-o', '--outputdir', help='Specify output directory')
-    parser.add_argument('-t', '--tvseries', action='store_true', help='Download all available in tv series')
-    parser.add_argument('-s', '--season', action='store_true', help='Download all available in the season')
-    parser.add_argument('-c', '--crontab', action='store_true',
-                        help='Setup a crontab for downloading the next episode when it releases')
-    parser.add_argument('-j', '--jobid', help='Cronjob id. Used for deleting old cronjobs')
+    subparsers = parser.add_subparsers(help='Sub-command help')
+
+    # Download subparser
+    dl_subparser = subparsers.add_parser('dl', help='Download episodes')
+    dl_subparser.add_argument('url', help='Url to video from dr.dk/tv')
+    dl_subparser.add_argument('-o', '--outputdir', help='Specify output directory')
+    dl_subparser.add_argument('-t', '--tvseries', action='store_true', help='Download all available in tv series')
+    dl_subparser.add_argument('-s', '--season', action='store_true', help='Download all available in the season')
+    dl_subparser.add_argument('--subscribe', action='store_true',
+                              help='Subscribe to the episode, season or series.')
+    dl_subparser.set_defaults(func=download)
+
+    # List subscriptions subparser
+    l_subscription_parser = subparsers.add_parser('lsubs', help='List subscriptions')
+    l_subscription_parser.set_defaults(func=list_subscriptions)
+
+    # Add subscription subparser
+    a_subscriptions_parser = subparsers.add_parser('add', help='Add new subscription')
+    a_subscriptions_parser.add_argument('url', help='Url to video from dr.dk/tv')
+    a_subscriptions_parser.add_argument('-o', '--outputdir', help='Specify output directory')
+    a_subscriptions_parser.add_argument('-t', '--tvseries', action='store_true', help='Subscribe to the tv series')
+    a_subscriptions_parser.add_argument('-s', '--season', action='store_true', help='Subscribe to the season')
+    a_subscriptions_parser.add_argument('-r', '--rate', default=2, type=int, nargs='?',
+                                        help='Set update rate to every x hours')
+    a_subscriptions_parser.set_defaults(func=add_subscription)
+
+    # Remove subscriptions subparser
+    r_subscriptions_parser = subparsers.add_parser('rsubs', help='Remove subscriptions')
+    r_subscriptions_parser.add_argument('subscriptions', metavar='int', type=int, nargs='+',
+                                        help='Index of subscription')
+    r_subscriptions_parser.set_defaults(func=remove_subscription)
+
+    # Upcoming releases subparser
+    upcoming_parser = subparsers.add_parser('upcoming', help='List upcoming episodes')
+    upcoming_parser.add_argument('url', help='Url to video from dr.dk/tv')
+    upcoming_parser.set_defaults(func=upcoming)
+
     args = parser.parse_args()
-    main(args)
+    args.func(args)
